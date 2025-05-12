0026a913f927f6c5e7206484850c74624edb1a4e
Kara1087 <46777855+Kara1087@users.noreply.github.com>
2025-05-12 15:44:17 +0200

    Regorganisation des scenes

--- Assets/_Ekko/Scripts/Core/LandingType.cs
+++
@@ -1,8 +0,0 @@
-// Assets/Scripts/Core/LandingType.cs
-
-public enum LandingType
-{
-    Normal,         // saut standard, aucun input spécial
-    Slam,           // appui sur bas = onde forte
-    Cushioned       // appui sur Shift/C = onde réduite
-}
